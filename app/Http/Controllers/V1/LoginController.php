b514eee577fe77425e0c5a241efb9f5df3d3e1c7
Alvin Tran <neverback88@gmail.com>
2016-04-07 15:24:28 +0700

    remove weird code

--- app/Http/Controllers/V1/LoginController.php
+++ app/Http/Controllers/V1/LoginController.php
@@ -7,22 +7,19 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Tymon\JWTAuth\JWTAuth;
 
-use App\Hocs\GroupMembers\GroupMemberRepository;
-
 class LoginController extends Controller
 {
 	private $jwtAuth;
-	private $gm;
 
-	public function __construct(JWTAuth $jwtAuth, GroupMemberRepository $gm)
+	public function __construct(JWTAuth $jwtAuth)
 	{
 	    $this->jwtAuth = $jwtAuth;
 	    $this->gm = $gm;
 	}
 
-	public function index()
-	{return $this->gm->index();
-		$credentials = ['email' => 'neverback88@gmail.com', 'password' => '4ever1love'];
+	public function index(Request $request)
+	{
+		$credentials = $request->only(['email', 'password']);
 		try {
 		    if (! $token = $this->jwtAuth->attempt($credentials)) {
 		        return $this->response->errorUnauthorized();
